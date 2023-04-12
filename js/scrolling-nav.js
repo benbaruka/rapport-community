18a1ddea5b75c1d2c2e41abe3eb8dca2210831e5
unknown <barukab68@gmail.com>
2023-04-12 07:43:12 -0700

    first commit

---
+++ js/scrolling-nav.js
@@ -0,0 +1,15 @@
+$(function () {
+  $("a.page-scroll").bind("click", function (event) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top,
+        },
+        1200,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
+  });
+});
